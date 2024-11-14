da0056698d5a2455cec5c4528832b5c3c7296ce1
Oleksandr Vlasov <drumlife182@gmail.com>
2024-11-14 21:26:59 +0100

    feat: added a new endpoint

--- models/product.model.js
+++ models/product.model.js
@@ -1,15 +1,31 @@
 const mongoose = require("mongoose");
 
 const ProductSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, "Name is required"],
-        },
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter product name"],
+    },
 
-        quality: {
-            type: Number,
-            required: true
-        }
-    }
-)
+    quality: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamp: true,
+  },
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+module.exports = Product;
